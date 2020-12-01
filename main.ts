ceab05531902f37e424ecf4d0906d32327791d77
sumonsheikh <sm.sumon117@gmail.com>
2020-12-02 00:08:03 +0600

    add some space in variable

--- main.ts
+++ main.ts
@@ -1,41 +1,42 @@
 class Student {
-    id : number;
-    name : string;
-    age : number;
-    gender? : string;
-    address? : string;
-    admissionMonth? : string;
-    course? : string;
+
+    id:                 number;
+    name:               string;
+    age:                number;
+    gender?:            string;
+    address?:           string;
+    admissionMonth?:    string;
+    course?:            string;
 
     constructor(
-        id : number,
-        name : string,
-        age : number,
-        gender? : string,
-        address? : string,
-        admissionMonth? : string,
-        course? : string,
+        id:                 number,
+        name:               string,
+        age:                number,
+        gender?:            string,
+        address?:           string,
+        admissionMonth?:    string,
+        course?:            string,
     ){
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
-        this.address = address;
+        this.id             = id;
+        this.name           = name;
+        this.age            = age;
+        this.gender         = gender;
+        this.address        = address;
         this.admissionMonth = admissionMonth;
-        this.course = course;
+        this.course         = course;
 
     };
 }
 
 
 class School {
-    studentAgeSum(studentAge1: number , studentAge2: number){
+    studentAgeSum(studentAge1: number, studentAge2: number){
         return (studentAge1 + studentAge2);
     }
 
 }
-let student1 = new Student( 1,'None',15,'None','None','None','None');
-let student2 = new Student( 2,'None',15,'None','None','None','None');
+let student1 = new Student(1,'None', 15, 'None', 'None', 'None', 'None');
+let student2 = new Student(2,'None', 15, 'None', 'None', 'None', 'None');
 
 let school = new School();
 let ageSum = school.studentAgeSum(student1.age, student2.age);
