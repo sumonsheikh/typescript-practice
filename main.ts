18d3b213de1b6b2d715c41852210357cfceefed4
sumonsheikh <sm.sumon117@gmail.com>
2020-12-03 20:24:56 +0600

    used interface for student class

--- main.ts
+++ main.ts
@@ -1,21 +1,29 @@
+interface IAddressDto{
+    id:         number;
+    areaName:   string;
+    postalCode: number;
+    city:       string;
+    country:    string;
+}
+
 class Student {
 
     id:                 number;
     name:               string;
     age:                number;
     gender?:            string;
-    address?:           string;
+    address?:           IAddressDto;
     admissionMonth?:    string;
-    course?:            string;
+    course?:            string[];
 
     constructor(
         id:                 number,
         name:               string,
         age:                number,
         gender?:            string,
-        address?:           string,
+        address?:           IAddressDto,
         admissionMonth?:    string,
-        course?:            string,
+        course?:            string[],
     ){
         this.id             = id;
         this.name           = name;
@@ -30,13 +38,14 @@ class Student {
 
 
 class School {
-    private studentAgeSum(studentAge1: number, studentAge2: number){
+    studentAgeSum(studentAge1: number, studentAge2: number){
         return (studentAge1 + studentAge2);
     }
 
-}
-let student1 = new Student(1,'None', 15, 'None', 'None', 'None', 'None');
-let student2 = new Student(2,'None', 15, 'None', 'None', 'None', 'None');
+}   
+
+let student1 = new Student(1,'Kabir', 15, 'male', studentAddress1, 'None');
+let student2 = new Student(2,'Khabir', 15, 'male', studentAddress2, 'None');
 
 let school   = new School();
 let ageSum   = school.studentAgeSum(student1.age, student2.age);
